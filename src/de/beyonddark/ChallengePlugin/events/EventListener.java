d352e6a7682f34d314d25972cf60106659fbb0c0
yan-luca Lentzen <yanluca.lentzen@gmail.com>
2020-07-22 17:29:20 +0200

    Updated EventListener with Inventory Events and Soup Healing

--- src/de/beyonddark/ChallengePlugin/events/EventListener.java
+++ src/de/beyonddark/ChallengePlugin/events/EventListener.java
@@ -4,19 +4,33 @@
 
 package de.beyonddark.ChallengePlugin.events;
 
+import de.beyonddark.ChallengePlugin.files.MainConfig;
+import de.beyonddark.ChallengePlugin.files.SpielregelConfig;
 import de.beyonddark.ChallengePlugin.inventorys.EinstellungInv;
 import de.beyonddark.ChallengePlugin.inventorys.HerausforderungInv;
+import de.beyonddark.ChallengePlugin.inventorys.Spielregel.SoupSettingsInv;
+import de.beyonddark.ChallengePlugin.inventorys.SpielregelInv;
 import de.beyonddark.ChallengePlugin.other.Scoreboard;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class EventListener implements Listener {
 
@@ -35,10 +49,39 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
+    public void onEntityDamage(EntityDamageEvent e) {
+        e.setCancelled(MainConfig.get().getBoolean("timerPaused"));
+    }
+
+    @EventHandler
+    public void onItemDamage(PlayerItemDamageEvent e) {
+        e.setCancelled(MainConfig.get().getBoolean("timerPaused"));
+    }
+
+    @EventHandler
+    public void onItemConsume(PlayerItemConsumeEvent e) {
+        e.setCancelled(MainConfig.get().getBoolean("timerPaused"));
+        if (!MainConfig.get().getBoolean("timerPaused")) {
+            if (MainConfig.get().getBoolean("Soup")) {
+                Material item = e.getItem().getType();
+                Player p = e.getPlayer();
+                int heal = SpielregelConfig.get().getInt("SoupHeal");
+                if (item == Material.MUSHROOM_STEW || item == Material.BEETROOT_SOUP || item == Material.RABBIT_STEW) {
+                    if (p.getHealth() + heal > 20) {
+                        p.setHealth(20);
+                    } else {
+                        p.setHealth(p.getHealth() + heal);
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) throws IOException {
         ItemStack whatWasClicked = e.getCurrentItem();
         Player p = (Player) e.getWhoClicked();
-        InventoryAction clickType = e.getAction();
+        ClickType clickType = e.getClick();
         assert whatWasClicked != null;
         if (e.getView().getTitle().equals("§6Einstellungen §8- §1Übersicht")) {
             if (whatWasClicked.getType() == Material.DRAGON_HEAD) {
@@ -52,6 +95,8 @@ public class EventListener implements Listener {
                 p.closeInventory();
             } else if (whatWasClicked.getType() == Material.MAP) {
                 p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
             }
             e.setCancelled(true);
         }
@@ -87,6 +132,63 @@ public class EventListener implements Listener {
             } else if (whatWasClicked.getType() == Material.EXPERIENCE_BOTTLE) {
                 p.closeInventory();
                 HerausforderungInv inv = new HerausforderungInv();
+                inv.openInventory(p);
+            } else if (whatWasClicked.getType() == Material.BARRIER) {
+                p.closeInventory();
+                EinstellungInv inv = new EinstellungInv();
+                inv.openInventory(p);
+            }
+            e.setCancelled(true);
+        }
+        if (e.getView().getTitle().equals("§6Einstellungen §8- §aSpielregeln")) {
+            if (whatWasClicked.getType().equals(Material.MUSHROOM_STEW)) {
+                p.closeInventory();
+                if (clickType.equals(ClickType.LEFT)) {
+                    if (SpielregelConfig.set().getBoolean("Soup")) {
+                        SpielregelConfig.set().set("Soup", false);
+                    } else if (!SpielregelConfig.get().getBoolean("Soup")) {
+                        SpielregelConfig.set().set("Soup", true);
+                    }
+                    SpielregelConfig.save();
+                    SpielregelInv inv = new SpielregelInv();
+                    inv.openInventory(p);
+                } else if (clickType.equals(ClickType.RIGHT)) {
+                    SoupSettingsInv inv = new SoupSettingsInv();
+                    inv.openInventory(p);
+                }
+            } else if (whatWasClicked.getType() == Material.MELON_SLICE) {
+                p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
+            } else if (whatWasClicked.getType() == Material.RED_TULIP) {
+                p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
+            } else if (whatWasClicked.getType() == Material.GOLDEN_APPLE) {
+                p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
+            } else if (Objects.requireNonNull(whatWasClicked.getItemMeta()).getDisplayName().equals("")) {
+                p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
+            } else if (whatWasClicked.getType() == Material.IRON_SWORD) {
+                p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
+            } else if (Objects.requireNonNull(whatWasClicked.getItemMeta()).getDisplayName().equals("")) {
+                p.closeInventory();
+                SpielregelInv inv = new SpielregelInv();
+                inv.openInventory(p);
+            } else if (whatWasClicked.getType() == Material.REDSTONE_LAMP) {
+                p.closeInventory();
+                if (SpielregelConfig.get().getBoolean("Schadensanzeige")) {
+                    SpielregelConfig.set().set("Schadensanzeige", false);
+                } else if (!SpielregelConfig.get().getBoolean("Schadensanzeige")) {
+                    SpielregelConfig.set().set("Schadensanzeige", true);
+                }
+                SpielregelConfig.save();
+                SpielregelInv inv = new SpielregelInv();
                 inv.openInventory(p);
             } else if (whatWasClicked.getType() == Material.BARRIER) {
                 p.closeInventory();
